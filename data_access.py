511a476de0714d835d6d848ef5959baa905bc771
Micaela Edith <romeromicaelaedith@gmail.com>
2024-04-10 13:47:31 -0300

    db addpost

--- data_access.py
+++ data_access.py
@@ -14,7 +14,7 @@ class DataAccess:
 
     def add_user(self, username, password):
         self.connect()
-        self.cursor.execute("INSERT INTO USERS (username, password) VALUES (?, ?)", (username, password))
+        self.cursor.execute("INSERT INTO USERS (username, password, admin) VALUES (?, ?, ?)", (username, password,0))
         self.connection.commit()
         print('guardado')
         self.disconnect()
@@ -26,12 +26,16 @@ class DataAccess:
         self.disconnect()
         return user
 
-    def add_post(self):
-	    pass
+    def add_post(self,  id_user, post, url_img, title, subtitle):
+	    self.connect()
+	    self.cursor.execute("INSERT INTO POST(id_user, post, url_img, title, subtitle) VALUES (?,?,?,?,?)", (id_user, post, url_img,title,subtitle))
+	    self.connection.commit()
+	    print("add post")
+	    self.disconnect()
 		
     def get_posts(self):
 	    self.connect()
-	    self.cursor.execute("SELECT * from POST")
+	    self.cursor.execute("SELECT * from POST order by id desc")
 	    all_posts = self.cursor.fetchall()
 	    self.disconnect()
 	    return all_posts	
--- main.py
+++ main.py
@@ -1,32 +1,53 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from data_access import DataAccess
 from post import Post
 
 app = Flask(__name__)
 
+loggin_ok=False
+id=1
 data= DataAccess()
-posts_list_db = data.get_posts()
-post_list = []
 
-for post in posts_list_db:
-	id= post[0]
-	id_user=post[1]
-	post_body=post[2]
-	url_img=post[3]
-	title=post[4]
-	subtitle= post[5]
-	aux=Post(id,id_user,post_body,url_img,title,subtitle)
-	post_list.append(aux)
+post_list = []
+post_list_final = []
 
 @app.route("/")
 def index():
+	posts_list_db = data.get_posts()
+	post_list.clear()
+	for post in posts_list_db:
+		id= post[0]
+		id_user=post[1]
+		post_body=post[2]
+		url_img=post[3]
+		title=post[4]
+		subtitle= post[5]
+		aux=Post(id,id_user,post_body,url_img,title,subtitle)
+		post_list.append(aux)
 	return render_template("index.html", posts = post_list)	
 	
 @app.route("/addPost")
 def add_post():
 	return render_template("add_post.html")	
 
-
+@app.route("/login")
+def login():
+	return render_template("login.html")
+	
+@app.route("/add_new_post", methods=["POST"])
+def add_new_post():
+		print("paso1")
+		id_user=id
+		title=request.form.get("title")
+		subtitle=request.form.get("subtitle")
+		post=request.form.get("textarea")
+		url_img="khgg"
+		print("paso")
+		data.add_post(id_user, post,url_img,title,subtitle)
+		return redirect("/")
+		
+		
+		
 
 if __name__=="__main__":
 	app.run(port=7000,debug=True)
